baf370284d1c94c9a04414cacf21cdafee3c4f1d
Polygaev <none@none>
2017-10-04 17:00:33 +0300

    packet: Added checks to header and data size

--- packet.c
+++ packet.c
@@ -12,8 +12,12 @@ struct packet_t create_packet(size_t packet_length) {
     static int packet_number = 1;
     static int header_size = sizeof(packet.number) + sizeof(packet.data_size) + sizeof(packet.crc32);
 
+    assert(header_size == PACKET_HEADER_SIZE);
+
     packet.number = packet_number++;
     packet.data_size = packet_length - header_size;
+
+    assert(packet.data_size >= 0);
 
     packet.data = generate_data(packet.data_size);
     packet.crc32  = crc32(0x00, packet.data, packet.data_size);
@@ -48,6 +52,8 @@ struct data_t packet_to_data(struct packet_t packet) {
 
     int header_size = sizeof(packet.number) + sizeof(packet.data_size) + sizeof(packet.crc32);
 
+    assert(header_size == PACKET_HEADER_SIZE);
+
     data.ptr = (unsigned char*)malloc(header_size + packet.data_size);
     if (data.ptr == NULL) {
         printf("packet_to_data: malloc() failed\n");
@@ -56,6 +62,7 @@ struct data_t packet_to_data(struct packet_t packet) {
 
     unsigned int offset = 0;
 
+    /* Copy header */
     memcpy((void*)(data.ptr + offset), (void*)&packet.number, sizeof(packet.number));
     offset += sizeof(packet.number);
 
@@ -65,6 +72,7 @@ struct data_t packet_to_data(struct packet_t packet) {
     memcpy((void*)(data.ptr + offset), (void*)&packet.crc32, sizeof(packet.crc32));
     offset += sizeof(packet.crc32);
 
+    /* Copy data */
     memcpy((void*)(data.ptr + offset), (void*)packet.data, packet.data_size);
     offset += packet.data_size;
 
@@ -76,6 +84,8 @@ struct data_t packet_to_data(struct packet_t packet) {
 struct packet_t packet_from_data(struct data_t data) {
     struct packet_t packet;
     int header_size = sizeof(packet.number) + sizeof(packet.data_size) + sizeof(packet.crc32);
+
+    assert(header_size == PACKET_HEADER_SIZE);
 
     unsigned char* buffer = (unsigned char*)malloc(data.size - header_size);
     if (buffer == NULL) {
